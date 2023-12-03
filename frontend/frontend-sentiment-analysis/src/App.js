45cbaa76ec6e2bb73962e28153f5540bec2e50d5
Numair Shaikh <numairsh77@gmail.com>
2023-12-03 22:05:46 +0530

    [Numair] Major UI changes to textbox and to display sentiment in a more interactive way.

--- frontend/frontend-sentiment-analysis/src/App.js
+++ frontend/frontend-sentiment-analysis/src/App.js
@@ -1,10 +1,45 @@
 import react from "react";
+import { Anchor } from 'antd';
+
 import MessageForm from "./Components/MessageForm/MessageForm";
 
 const App = () => {
   return (
-    <div>
-      <MessageForm />
+    <div style={{ background: '#FFF6F6', width: '100%'}}>
+      <div>
+        <Anchor
+          direction="horizontal"
+          style={
+              { 
+                background: '#FAF3F0', 
+                borderRadius: '3px',
+                padding: '10px',
+                borderBottomColor: 'black',
+                borderBottomWidth: '2px'
+              }
+            }
+          items={
+            [
+              {
+                key: 'project-title',
+                title: <h1 style={
+                    {
+                      fontSize: '24px', 
+                      margin: 0,
+                      fontWeight: 'normal' 
+                    }
+                  }
+                >
+                  Sentiment Analysis on Cryptocurrency Tweets
+                </h1>,
+              },
+            ]
+          }
+        />
+      </div>
+      <div>
+        <MessageForm />
+      </div>
     </div>
   );
 }
--- frontend/frontend-sentiment-analysis/src/Components/MessageForm/MessageForm.js
+++ frontend/frontend-sentiment-analysis/src/Components/MessageForm/MessageForm.js
@@ -24,31 +24,97 @@ const MessageForm = () => {
 
     return (
         <div className="input-form">
-            <h1 className="description-ac">Sentiment Analysis of Crypto Tweets</h1>
-            <p className="description-ac">Please Enter the tweet for which the sentiment is to be analyzed.</p>
+            <p className="description">Please enter the tweet for which the sentiment is to be analyzed.</p>
             <Form 
                 name="basic"
                 labelCol={{ span: 8 }}
                 wrapperCol={{ span: 16 }}
                 initialValues={{ remember: true }}
             >
-                <Form.Item
-                    label="tweet"
-                    name="tweet"
-                    rules = {[{required: true, message:'Please enter the tweet for which the sentiment is to be analyzed!'}]}
+                <Form.Item 
+                    label="Tweet"
+                    name="Tweet"
+                    rules = {
+                        [
+                            {
+                                required: true, 
+                                message:'Please enter the tweet for which the sentiment is to be analyzed!'
+                            }
+                        ]
+                    }
+                    style={
+                        { 
+                            width: '80%'
+                        }
+                    }
                 >
-                    <Input placeholder="Tweet" onChange={e => setMessage(e.target.value)}/>
+                    <Input.TextArea style={
+                            { 
+                                width: '80%',
+                                height: '150px'
+                            }
+                        } 
+                        allowClear
+                        showCount 
+                        placeholder="Tweet to be analyzed." 
+                        onChange={
+                            e => setMessage(e.target.value)
+                        }
+
+                    />
                 </Form.Item>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" htmlType="submit" onClick={(event) => {
-                        event.preventDefault()
-                        callBackend()
-                    }}>
+
+                <Form.Item 
+                    wrapperCol={
+                        { 
+                            offset: 8, 
+                            span: 16 
+                        }
+                    }
+                >
+                    <Button 
+                        type="primary" 
+                        htmlType="submit" 
+                        onClick={(event) => {
+                            event.preventDefault()
+                            callBackend()
+                        }}
+                        size="large"
+                    >
                         Calculate
                     </Button>
                 </Form.Item>
             </Form>
-            {output?<p className="output">The overall sentiment of the tweet is <b>{output}</b></p>:<></>}
+            {/* {output?<p className="output">The overall sentiment of the tweet is <b>{output}</b></p>:<></>} */}
+            {
+                output !== undefined ? (
+                    <p className="output" style={
+                            { 
+                                backgroundColor: output === '0' ? '#FF6969' : output === '1' ? '#FFF5C2' : '#C7DCA7',
+                                padding: '10px',
+                                color: 'white',
+                                width: '50%',
+                                borderRadius: '5px'
+                            }
+                        }
+                    > 
+                        <b style={
+                                { 
+                                    color: output === '0' ? 'white' : output === '1' ? '#45474B' : '#45474B',
+                                    padding: '5px',
+                                }
+                            }
+                        >
+                        The overall sentiment of the tweet is
+                            {
+                                output === '0' ? " negative." : output === '1' ? " neutral." : " positive."
+                            }
+                        </b>
+                    </p>        
+                ) : (
+                    <></>
+                )
+}
         </div>
     );
 }
